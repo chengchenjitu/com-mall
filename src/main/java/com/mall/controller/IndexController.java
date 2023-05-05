eb15962047e5bc6e9581c91f69e5277ca25795ee
chengchenjitu <1639669423@qq.com>
2023-05-05 11:24:08 +0800

    Initial commit 完成框架搭建

---
+++ src/main/java/com/mall/controller/IndexController.java
@@ -0,0 +1,27 @@
+package com.mall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/","/index"})
+public class IndexController {
+
+    @RequestMapping({"/","/index"})
+    public String index(){
+        return "index/index";
+    }
+
+
+
+    @RequestMapping("/login")
+    public String login(){
+        return "index/login";
+    }
+
+    @RequestMapping("/home")
+    public String home(){
+        return "index/home";
+    }
+
+}
